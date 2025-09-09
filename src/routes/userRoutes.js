866fa8c9a6273b868bcd8c313e59ea7b00922ec0
RasikaJade1 <jaderasika4@gmail.com>
2025-09-09 21:14:20 +0530

    implemented user authentication and authorization

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,20 +1,22 @@
 const express = require("express");
+const verifyToken = require("../middlewares/authMiddleware");
+const authorizeRoles = require("../middlewares/roleMiddleware");
 
 const router = express.Router();
 
 //Only admins can access this router
-router.get("/admin", (req,res) => {
+router.get("/admin", verifyToken, authorizeRoles("admin"),(req,res) => {
     res.json({ message: "Welcome Admin"});
 });
 
 //Both admin and lawyer can access this router
-router.get("/lawyer", (req,res) => {
+router.get("/lawyer",verifyToken, authorizeRoles("admin", "lawyer"), (req,res) => {
     res.json({ message: "Welcome Lawyer"});
 });
 
 
 //All can access this router
-router.get("/client", (req,res) => {
+router.get("/client",verifyToken,authorizeRoles("admin", "client"), (req,res) => {
     res.json({ message: "Welcome Client"});
 });
 
